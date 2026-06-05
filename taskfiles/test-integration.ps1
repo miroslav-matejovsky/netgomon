a62dd1f54d1e38a502fe1d2bab098694de0f7529
Miroslav Matejovsky <miroslav.matejovsky@gmail.com>
2026-06-05 05:02:05 +0200

    simpler integration tests

--- taskfiles/test-integration.ps1
+++ taskfiles/test-integration.ps1
@@ -7,8 +7,13 @@ if (-not (Test-Path $outDir)) {
     New-Item -ItemType Directory -Path $outDir | Out-Null
 }
 
+Write-Host "Cleaning and building probe.exe first since integration tests depend on it..."
+task clean QUIET=1
+task build-probe
+
+Write-Host "Running integration tests ..."
 Write-Host "saving output to $outFile"
 
-gotestsum --format pkgname -- -tags integration ./... 2>&1 | Tee-Object -FilePath $outFile
+gotestsum --format pkgname -- ./internal/etw/goetw/ ./internal/etw/rawsec/ ./internal/iphelper/ -v -run Integration 2>&1 | Tee-Object -FilePath $outFile
 
 exit $LASTEXITCODE
